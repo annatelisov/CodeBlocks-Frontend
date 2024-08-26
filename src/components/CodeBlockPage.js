a8c225930d68548943e27dd3898c4b422f6cf7db
Anna Telisov <annatelisov@gmail.com>
2024-08-26 17:02:19 +0300

    update backend url

--- src/components/CodeBlockPage.js
+++ src/components/CodeBlockPage.js
@@ -12,10 +12,10 @@ function CodeBlockPage() {
   const [students, setStudents] = useState(0);
   const { id } = useParams();
   const navigate = useNavigate();
-  const socket = io('https://web-production-abd4.up.railway.app/');
+  const socket = io('https://web-production-abd4.up.railway.app');
   useEffect(() => {
     //Define this again because this didn't work in other way even when return socket
-    const socket = io('https://web-production-abd4.up.railway.app/');
+    const socket = io('https://web-production-abd4.up.railway.app');
 
     //Fetch data from the server and db, localhost:5000 for localhost
     axios.get(`https://web-production-abd4.up.railway.app/api/codeblocks/${id}`)
@@ -25,10 +25,10 @@ function CodeBlockPage() {
         setSolution(response.data.solution);
       })
       .catch(error => console.error('Error fetching code block:', error));
-      
+
     //Join the block room via socket
     socket.emit('joinBlock', id);
-    
+
 
     //Listen for role assignment and set true if this is the mentor or false if this is a student
     socket.on('roleAssignment', (role) => {
@@ -56,7 +56,7 @@ function CodeBlockPage() {
       socket.disconnect();
     };
   }, [id, navigate]);
-  
+
 
   //Updating code changes in the textarea
   const handleCodeChange = (e) => {
